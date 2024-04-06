49cc5cd7dd8046403bb69e718db926220c7af747
Harpreet Singh Gulati <harpreet.surdy@gmail.com>
2024-04-05 19:00:51 -0700

    Create setup-openwrt.sh

---
+++ setup-openwrt.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Remove old img if it exists
+rm -f *.img
+
+# Download and extract openwrt x86_64 img
+curl -O https://downloads.openwrt.org/releases/23.05.3/targets/x86/64/openwrt-23.05.3-x86-64-generic-ext4-combined-efi.img.gz
+gunzip openwrt-23.05.3-x86-64-generic-ext4-combined-efi.img.gz
+
+# Flash the image to disk
+sudo dd if=openwrt-23.05.3-x86-64-generic-ext4-combined-efi.img of=/dev/sda
+
+# Fix partition table
+printf 'p\nw\n' | sudo fdisk /dev/sda
+
+#Grow root partition to fill the disk
+sudo growpart /dev/sda 2
+sudo resize2fs /dev/sda2
+
+#Create files and dirs for openwrt chroot
+sudo mkdir -p /mount/openwrt
+sudo mount -t proc /proc /mount/openwrt/proc/
+sudo mount --rbind /sys /mount/openwrt/sys/
+sudo mount --rbind /dev /mount/openwrt/dev/
+echo "nameserver 8.8.8.8" > resolv.conf
+sudo mount /dev/sda2 /mount/openwrt
+sudo mkdir -p /mount/openwrt/tmp/lock
+sudo mv resolv.conf /mount/openwrt/tmp/resolv.conf
